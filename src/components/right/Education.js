f8bd6f4c1fc737013a294f11ac87072e1542d198
Ravindran Abilash <e18013@eng.pdn.ac.lk>
2023-02-18 15:18:29 +0530

    create the hide and show for each section

--- src/components/right/Education.js
+++ src/components/right/Education.js
@@ -1,9 +1,39 @@
-import {Box} from "@mui/material";
+import {Box, Stack, Typography} from "@mui/material";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import List from "@mui/material/List";
+import ExperienceCard from "../cards/ExperienceCard";
 
 const Education = () => {
     return (
         <Box bgcolor="white">
-            Education
+
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography variant="h6" padding={1}>Education</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Stack direction="column">
+                        <List
+                            sx={{
+                                width: '98%',
+                            }}
+                        >
+                            <ExperienceCard/>
+                            <ExperienceCard/>
+                            <ExperienceCard/>
+                        </List>
+
+                    </Stack>
+                </AccordionDetails>
+            </Accordion>
+
         </Box>
     )
 }
--- src/components/right/Experience.js
+++ src/components/right/Experience.js
@@ -3,22 +3,39 @@ import List from '@mui/material/List';
 
 import ExperienceCard from "../cards/ExperienceCard";
 
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 const Experience = () => {
     return (
         <Box bgcolor="white">
-            <Typography variant="h6" padding={1}>Experience</Typography>
-            <Stack direction="column">
-                <List
-                    sx={{
-                        width: '98%',
-                    }}
-                >
-                    <ExperienceCard/>
-                    <ExperienceCard/>
-                    <ExperienceCard/>
-                </List>
 
-            </Stack>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography variant="h6" padding={1}>Experience</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Stack direction="column">
+                        <List
+                            sx={{
+                                width: '98%',
+                            }}
+                        >
+                            <ExperienceCard/>
+                            <ExperienceCard/>
+                            <ExperienceCard/>
+                        </List>
+
+                    </Stack>
+                </AccordionDetails>
+            </Accordion>
+
         </Box>
     )
 }
